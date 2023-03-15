707cefc8c53a2e3a3aca4ae234faa6b21c2978e9
Marcus Henrik Simonsen <mhsi@itu.dk>
2023-03-15 21:54:36 +0100

    Add Pi approx. using dots

--- public/js/p5_projects/pi_approximator.js
+++ public/js/p5_projects/pi_approximator.js
@@ -1,8 +1,35 @@
+let points = 0;
+let pointsInside = 0;
+let piDiv;
+
 function setup() {
     var canvas = createCanvas(500, 500);
     canvas.parent('p5_canvas');
+
+    piDiv = select('#pi_div');
+
+    background(0);
+    stroke(255);
+    strokeWeight(2);
+    fill(0);
+    ellipse(width/2, height/2, width);
 }
 
 function draw() {
-    background(0);
+    strokeWeight(1);
+    for (let i = 0; i < 1000; i++) {
+        let x = random(width);
+        let y = random(height);
+
+        let distFromCenter = sqrt((width/2-x)*(width/2-x) + (height/2-y)*(height/2-y));
+
+        stroke(color(0, 0, 255));
+        points++;
+        if (distFromCenter < width/2) {
+            pointsInside++;
+            stroke(color(0, 255, 0));
+        }
+        point(x, y);
+    }
+    piDiv.html(4*pointsInside/points);
 }
